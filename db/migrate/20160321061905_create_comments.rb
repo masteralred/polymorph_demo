abe997b9c066b7607765a768c84d41b2d52ce451
Alexander Zhadnov <masteralred@gmail.com>
2016-03-21 09:58:11 +0300

    Add polymorphic association Post, Image --> Comment

--- db/migrate/20160321061905_create_comments.rb
+++ db/migrate/20160321061905_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :content
+      t.references :commentable, polymorphic: true, index: true
 
       t.timestamps null: false
     end
